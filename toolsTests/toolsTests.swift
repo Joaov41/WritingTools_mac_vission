83bc572a26f506411da687ccbb6bfb5ab2e54a0f
Joaov41 <52511571+Joaov41@users.noreply.github.com>
2025-01-22 21:33:07 +0000

    Add files via upload

---
+++ toolsTests/toolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  toolsTests.swift
+//  toolsTests
+//
+//  Created by john val on 1/16/25.
+//
+
+import Testing
+@testable import tools
+
+struct toolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
